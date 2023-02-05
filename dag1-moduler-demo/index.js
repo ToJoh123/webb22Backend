648aa20e66a52b93573f4ac5cc1543c1f9dcc005
Metin Ahmad <metin.ahmad@paligo.net>
2023-02-05 11:36:11 +0100

    First commit

---
+++ dag1-moduler-demo/index.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+const additionResult = math.add(5, 5);
+const multiplicationResult = multiply(5, 5);
+const divisionResult = divide(5, 5);
+
+console.log(additionResult);
+console.log(multiplicationResult);
+console.log(divisionResult);
