0047533689f1558ee9b415e216f9c4b08aa14aaf
Metin Ahmad <metin.ahmad@paligo.net>
2023-02-28 11:14:12 +0100

    Added dag7 validation

--- dag7-databas-och-validering/server/src/index.js
+++ dag7-databas-och-validering/server/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const server = express();
 const mysql = require('mysql2');
+const joi = require('joi');
+server.use(express.json());
 
 const config = {
     user: 'root',
@@ -12,15 +14,43 @@ const config = {
 const pool = mysql.createPool(config);
 
 server.get('/', (req, res) => {
-    pool.execute('SELECT * FROM users WHERE username=?', ['metin'] ,(error, result) => {
+    const schema = joi.object({
+        username: joi.string().min(3).max(20).required()
+    });
+
+    const validation = schema.validate(req.query);
+
+    if (validation.error) {
+        return res.status(400).json(validation.error.details[0].message);
+    }
+
+    const {username} = validation.value;
+
+
+    pool.execute('SELECT * FROM users WHERE username=?', [username] ,(error, result) => {
         if (error) {
             console.log(error);
             res.sendStatus(500);
         } else {
             res.json(result);
         }
-    })
-
+    });
 });
+
+server.post('/', (req, res) => {
+    const { username, password, email, favoriteCandy } = req.body;
+    
+    console.log(req.body);
+    console.log({username, password, email, favoriteCandy});
+
+    const schema = joi.object({
+        username: joi.string().min(3).max(20).required(),
+        password: joi.string().min(3).max(15).required(),
+        email: joi.string().email().required(),
+        favoriteCandy: joi.string()
+    });
+
+    const validation = schema.validate(req.body);
+})
 
 server.listen(5050);
